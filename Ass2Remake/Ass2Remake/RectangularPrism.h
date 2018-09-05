9a1fa2dd7dca0b709f72689ba1017fa7c4f7f5b7
meiyan1712 <42823657+meiyan1712@users.noreply.github.com>
2018-09-05 18:11:20 +1000

    Update RectangularPrism.h

--- Ass2Remake/Ass2Remake/RectangularPrism.h
+++ Ass2Remake/Ass2Remake/RectangularPrism.h
@@ -3,6 +3,8 @@
 
 //Code written by: Mei Yan Tang (z5129009)
 
+//Class is derived by 'Shape' class that defines a Rectangular Prism.
+//variables were obtained by the shape specification from Moodle.
 class RectangularPrism : public Shape
 {
 private:
