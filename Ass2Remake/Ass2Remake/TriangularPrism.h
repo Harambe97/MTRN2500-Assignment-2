80bd9191336c5748f0ea6c62bd5e249c2b044053
meiyan1712 <meiyan171298@gmail.com>
2018-09-05 10:51:51 +1000

    Added remade Assignment files due to crash

---
+++ Ass2Remake/Ass2Remake/TriangularPrism.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Shape.hpp"
+
+//Code written by: Mei Yan Tang (z5129009)
+
+class TriangularPrism : public Shape
+{
+private:
+	double a_length;
+	double b_length;
+	double depth;
+	double theta;
+
+public:
+	TriangularPrism(double x_, double y_, double z_, double rotation_, double length_a, double length_b, double length_d, double angle);
+	void draw();
+};
+
